105094457565f57c7ce9faa3f4aa0ede2ab5d4d8
vava <Haisv@MSI>
2025-09-13 21:35:07 -0600

    while.

--- src/Main/main.java
+++ src/Main/main.java
@@ -1,9 +1,20 @@
 package Main;
 
+import java.util.Scanner;
+
 public class main {
+    public static Scanner MyScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
-    }
+        int n = 0;
+        while(true){
+            System.out.print("Ingresa -1 para salir: ");
+            n = MyScanner.nextInt();
+            if(n == -1){
+                break;
+            }
+            System.out.println("Decidiste continuar.");
+        }//Fin del while
+    }//Fin del main
     
 }
