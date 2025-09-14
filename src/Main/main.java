78fa69948a138be49d4c42623f491f72609a58c5
vava <Haisv@MSI>
2025-09-13 21:07:31 -0600

    Hola mundo

--- src/Main/main.java
+++ src/Main/main.java
@@ -5,7 +5,7 @@
 package Main;
 
 /**
- *
+ * 
  * @author Haisv
  */
 public class main {
@@ -15,6 +15,7 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
